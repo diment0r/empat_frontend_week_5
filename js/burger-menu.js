841d9c2b444342ae0f822b08dec2eb485cb41806
dimentor <dmitriyprudnik.workmail@gmail.com>
2024-03-31 13:22:20 +0300

    contacts page adaptive

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -2,4 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('burger').addEventListener('click', function () {
         document.querySelector('header').classList.toggle('active');
     });
+
+    let url = window.location.href;
+    if(url.split('/')[url.split('/').length - 1] == 'programs-page.html') {
+        document.querySelectorAll('.header__menu-item a')[0].classList.toggle('red');
+    } else if(url.split('/')[url.split('/').length - 1] == 'contacts-page.html') {
+        document.querySelectorAll('.header__menu-item a')[1].classList.toggle('red');
+    }
 });
